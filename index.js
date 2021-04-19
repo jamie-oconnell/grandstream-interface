1593baf17af56bd71d19b83d5e0c8feb608fd76b
Jamie O'Connell <it@nbn.local>
2021-04-19 15:02:08 +1000

    Inital network scanning

---
+++ index.js
@@ -0,0 +1,64 @@
+const find = require("local-devices");
+fs = require("fs");
+
+(function () {
+  var ConvertBase = function (num) {
+    return {
+      from: function (baseFrom) {
+        return {
+          to: function (baseTo) {
+            return parseInt(num, baseFrom).toString(baseTo);
+          },
+        };
+      },
+    };
+  };
+
+  // binary to decimal
+  ConvertBase.bin2dec = function (num) {
+    return ConvertBase(num).from(2).to(10);
+  };
+
+  // binary to hexadecimal
+  ConvertBase.bin2hex = function (num) {
+    return ConvertBase(num).from(2).to(16);
+  };
+
+  // decimal to binary
+  ConvertBase.dec2bin = function (num) {
+    return ConvertBase(num).from(10).to(2);
+  };
+
+  // decimal to hexadecimal
+  ConvertBase.dec2hex = function (num) {
+    return ConvertBase(num).from(10).to(16);
+  };
+
+  // hexadecimal to binary
+  ConvertBase.hex2bin = function (num) {
+    return ConvertBase(num).from(16).to(2);
+  };
+
+  // hexadecimal to decimal
+  ConvertBase.hex2dec = function (num) {
+    return ConvertBase(num).from(16).to(10);
+  };
+
+  this.ConvertBase = ConvertBase;
+})(this);
+
+find("172.27.5.1-172.27.7.250").then((devices) => {
+  const newDevices = devices.map((device) => {
+    const removedColon = device.mac.replace(/:/g, "");
+    const last2mac = removedColon.substring(removedColon.length - 2);
+    console.log(last2mac);
+    const deviceLast2Mac = ConvertBase.hex2dec(last2mac) - 1;
+    const newDeviceLast2Mac = ConvertBase.dec2hex(deviceLast2Mac);
+
+    const newDevice = removedColon.slice(0, -2) + newDeviceLast2Mac;
+    return { ip: device.ip, original_mac: device.mac, mac: newDevice };
+  });
+  fs.writeFile("devices.json", JSON.stringify(newDevices), function (err) {
+    if (err) return console.log(err);
+  });
+});
